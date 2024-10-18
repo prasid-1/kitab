2f2a6deacb18d6e50ebf3c2ea36624a26b3c3284
stelmon01 <stelmon01@gmail.com>
2024-10-18 17:37:34 +0545

    edit

--- maduskitab/lib/main.dart
+++ maduskitab/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+// import 'package:provider/provider.dart';
 import 'my_app.dart';
-import 'app_state.dart';
+// import 'app_state.dart';
 
 void main() {
   runApp(MyApp());
--- maduskitab/test/widget_test.dart
+++ maduskitab/test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:maduskitab/main.dart';
+// import 'package:maduskitab/main.dart';
 
 //import 'maduskitab/main.dart';
 
